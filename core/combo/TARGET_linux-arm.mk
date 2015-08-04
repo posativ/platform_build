cb34ab0e6c173a82f8e16f86af030743277d8205
Daniel Micay <danielmicay@gmail.com>
2015-08-04 19:59:25 -0400

    default to -fwrapv to work around broken code
    The issue of broken signed overflow checks being removed by (valid)
    compiler optimizations is fairly common. Default to sane language
    semantics instead of the strict interpretation of the standard.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -100,6 +100,7 @@ $(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += \
 			-fdata-sections \
 			-funwind-tables \
 			-fstack-protector \
+			-fwrapv \
 			-Wa,--noexecstack \
 			-Werror=format-security \
 			-D_FORTIFY_SOURCE=2 \
