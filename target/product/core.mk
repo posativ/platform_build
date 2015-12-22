49412906335556a2b0beb36076c759cf98ed25ce
Daniel Micay <danielmicay@gmail.com>
2015-12-21 22:02:09 -0500

    replace Browser app with Chromium

--- target/product/core.mk
+++ target/product/core.mk
@@ -22,7 +22,6 @@
 
 PRODUCT_PACKAGES += \
     BasicDreams \
-    Browser \
     Calendar \
     CalendarProvider \
     CaptivePortalLogin \
@@ -53,6 +52,7 @@ PRODUCT_PACKAGES += \
     Telecom \
     TeleService \
     VpnDialogs \
-    MmsService
+    MmsService \
+    chromium
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
