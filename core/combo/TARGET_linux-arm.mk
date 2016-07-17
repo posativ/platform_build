e061b03ba28fdda0a26a8970b7fa51ce5ce21b9c
Daniel Micay <danielmicay@gmail.com>
2016-07-17 00:35:47 -0400

    add -Wvla -Wno-error=vla
    Change-Id: Ia9cbb0cc6ed8e3f8791c792b88c29f3f04f2e244

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -106,6 +106,7 @@ $(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += \
 			-Wsuggest-attribute=format \
 			-Wno-error=suggest-attribute=format \
 			-Werror=format-security \
+			-Wvla -Wno-error=vla \
 			-D_FORTIFY_SOURCE=2 \
 			-fno-short-enums \
 			-no-canonical-prefixes \
--- core/combo/TARGET_linux-arm64.mk
+++ core/combo/TARGET_linux-arm64.mk
@@ -83,6 +83,7 @@ TARGET_GLOBAL_CFLAGS += \
 			-Wsuggest-attribute=format \
 			-Wno-error=suggest-attribute=format \
 			-Werror=format-security \
+			-Wvla -Wno-error=vla \
 			-D_FORTIFY_SOURCE=2 \
 			-fno-short-enums \
 			-no-canonical-prefixes \
