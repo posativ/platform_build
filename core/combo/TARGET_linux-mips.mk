e93f8ef4e1a02f78f8576f21285ade53137369de
Daniel Micay <danielmicay@gmail.com>
2016-07-17 00:35:47 -0400

    add -Wvla -Wno-error=vla
    Change-Id: Ia9cbb0cc6ed8e3f8791c792b88c29f3f04f2e244

--- core/combo/TARGET_linux-mips.mk
+++ core/combo/TARGET_linux-mips.mk
@@ -93,6 +93,7 @@ $(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += \
 			-Wsuggest-attribute=format \
 			-Wno-error=suggest-attribute=format \
 			-Werror=format-security \
+			-Wvla -Wno-error=vla \
 			-D_FORTIFY_SOURCE=2 \
 			-no-canonical-prefixes \
 			-fno-canonical-system-headers \
